de4f19957e8f878e1e4aa656ae8a0e27548c5bf6
Silvia1303 <silvia.hasibeder@gmx.at>
2026-03-01 13:42:45 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+var CACHE_NAME = 'med-ablauf-v1';
+var urlsToCache = ['./', './medikamenten-ablauf.html'];
+self.addEventListener('install', function(event) { event.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(urlsToCache); })); self.skipWaiting(); });
+self.addEventListener('activate', function(event) { event.waitUntil(caches.keys().then(function(cacheNames) { return Promise.all(cacheNames.map(function(cacheName) { if (cacheName !== CACHE_NAME) { return caches.delete(cacheName); } })); })); self.clients.claim(); });
+self.addEventListener('fetch', function(event) { event.respondWith(caches.match(event.request).then(function(response) { if (response) { return response; } return fetch(event.request).then(function(response) { if (!response || response.status !== 200) { return response; } var responseToCache = response.clone(); caches.open(CACHE_NAME).then(function(cache) { cache.put(event.request, responseToCache); }); return response; }).catch(function() { return new Response('Offline - bitte spaeter erneut versuchen.'); }); })); });
